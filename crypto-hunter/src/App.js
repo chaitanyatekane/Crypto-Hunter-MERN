3a38607f4bd23508e27e1550dd5961beb62a82ea
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-10-19 21:52:44 +0530

    Created Page Routes

--- crypto-hunter/src/App.js
+++ crypto-hunter/src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Coinpage from "./pages/Coinpage";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello Chaitanya</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={Coinpage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
