5b70ec3276144fb8c90fd0caec98596c41a8a6ed
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-10-19 22:24:35 +0530

    Update Header & Dark theme

--- crypto-hunter/src/components/Header.js
+++ crypto-hunter/src/components/Header.js
@@ -1,7 +1,70 @@
+import {
+  AppBar,
+  Container,
+  createTheme,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+  ThemeProvider,
+} from "@material-ui/core";
+import { dark } from "@material-ui/core/styles/createPalette";
+import { makeStyles } from "@material-ui/styles";
 import React from "react";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
 
 const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push("/")}
+              className={classes.title}
+              variant="h6"
+            >
+              Crypto Hunter
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
 export default Header;
